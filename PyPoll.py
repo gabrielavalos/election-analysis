68ecb1c6728b067572bf2eeefaf0e2c0c6ea8942
Gabriela E Avalos <gabrielaeavalos@gabrielas-air.home>
2021-01-10 13:01:33 -0600

    updates PyPoll with all results

--- PyPoll.py
+++ PyPoll.py
@@ -108,7 +108,9 @@ with open(path_ElectionResults) as data_analysis:
             f'Winning Vote Count: {winning_count:,}\n'
             f'Winning Percentage: {winning_percentage:.1f}%\n'
             f'-------------------------\n')
-        #print( winning_candidate_summary)
+        print( winning_candidate_summary)
+        # Save the winning candidate's results to the text file
+        saved_file.write(winning_candidate_summary)
            
     
     #print(total_votes)
